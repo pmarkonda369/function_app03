db25fc40b5e2913c0d7fba5c10c7d922f5d4ff34
pradeep <saipradeep.net@gmail.com>
2024-11-11 20:59:02 +0530

    fdsf

--- api/HttpTrigger1/__init__.py
+++ api/HttpTrigger1/__init__.py
@@ -1,24 +1,23 @@
-import logging
-
 import azure.functions as func
-
+import logging
+import json
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    # Try to get 'query' from request parameters or JSON body
     query = req.params.get('query')
     if not query:
         try:
             req_body = req.get_json()
+            query = req_body.get('query')
         except ValueError:
             pass
-        else:
-            name = req_body.get('query')
 
     if query:
         return func.HttpResponse(f"Hello, {query}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
         )
